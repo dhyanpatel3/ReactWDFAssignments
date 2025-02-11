0ca1713f68661d66cf129781c426ae54ffd82c22
dhyanpatel3 <dp28dhyan@gmail.com>
2025-02-11 22:03:55 +0530

    added task4

---
+++ task4/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import WelcomeMessage from './components/WelcomeMessage'
+import "./App.css"
+
+export default function App() {
+  return (
+    <>
+     <WelcomeMessage/> 
+    </>
+  )
+}
